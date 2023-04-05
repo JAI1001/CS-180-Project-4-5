a308864b33001760eb855b12f5b2c0bb9f1e59d9
janeb10 <53449342+janeb10@users.noreply.github.com>
2023-04-05 12:46:19 -0400

    Updated Class
    added getters/setters

--- ProjectFolder/src/Product.java
+++ ProjectFolder/src/Product.java
@@ -7,7 +7,10 @@ public class Product {
     private String seller;
     private String storeName;
 
-    public Product(String name, double price, int quantity, int quantitySold, String seller, String storeName){
+
+    //constructor
+
+    public Product(String name, double price, int quantity, int quantitySold, String seller, String storeName) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -16,9 +19,49 @@ public class Product {
         this.storeName = storeName;
 
 
-
     }
-    //getters setters for name, price, quantity, quantity sold, seller, store name
 
-    //product list extends this class
-    //description
+    //getters
+
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getQuantity(){
+        return  quantity;
+    }
+    public int getQuantitySold(){
+        return  quantitySold;
+    }
+    public String getSeller(){
+        return seller;
+    }
+    public String getStoreName(){
+        return storeName;
+    }
+
+    //setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public void setQuantitySold(int quantitySold){
+        this.quantitySold = quantitySold;
+    }
+    public void setSeller(String seller){
+        this.seller = seller;
+    }
+    public void setStoreName(String storeName){
+        this.storeName = storeName;
+    }
+
+
+//product list extends this class
+//description
