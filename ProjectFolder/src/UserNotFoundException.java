4a6e864703868bd01d32f3042a9deff95b44e6b6
Jai <102352192+JAI1001@users.noreply.github.com>
2023-04-10 14:48:10 -0400

    Update UserNotFoundException.java
    Reformatted Version

--- ProjectFolder/src/UserNotFoundException.java
+++ ProjectFolder/src/UserNotFoundException.java
@@ -1,4 +1,16 @@
 import java.io.Serializable;
+
+/**
+ * Project-4 Marketplace
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2023 -- Project-4</p>
+ *
+ * @version April 10, 2023
+ *
+ * @author: Seungjae Baik, Jane Billa, Thomas Birk, Kuan-Yu Chen, Jai Nanda
+ */
+
+
 public class UserNotFoundException extends Exception {
     public UserNotFoundException(String message) {
         super(message);
