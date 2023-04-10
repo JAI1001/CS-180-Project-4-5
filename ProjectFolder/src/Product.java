2d7c831063dd0e0c4fbd1e641d258177b616c808
Jai <102352192+JAI1001@users.noreply.github.com>
2023-04-10 14:46:46 -0400

    Update Product.java
    Reformatted Version

--- ProjectFolder/src/Product.java
+++ ProjectFolder/src/Product.java
@@ -1,3 +1,13 @@
+/**
+ * Project-4 Marketplace
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2023 -- Project-4</p>
+ *
+ * @version April 10, 2023
+ *
+ * @author: Seungjae Baik, Jane Billa, Thomas Birk, Kuan-Yu Chen, Jai Nanda
+ */
+
 public class Product {
 
     private String name;
