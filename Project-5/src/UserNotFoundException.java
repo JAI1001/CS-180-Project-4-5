ba4900ba79a4cabe8541337d2e54e5b195b8319a
Jai <102352192+JAI1001@users.noreply.github.com>
2023-05-01 17:10:33 -0400

    Add files via upload

---
+++ Project-5/src/UserNotFoundException.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+/**
+ * Project-5 Marketplace
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2023 -- Project-4</p>
+ *
+ * @version April 10, 2023
+ * @author: Seungjae Baik, Jane Billa, Thomas Birk, Kuan-Yu Chen, Jai Nanda
+ */
+
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
+
